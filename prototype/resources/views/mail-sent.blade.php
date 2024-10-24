7a809ff6982677d06e759467e2e2d5832b31d2b0
ChabbehAymen <devaymen15.5.3@gmail.com>
2024-10-24 22:57:26 +0100

    added some css to the mail tmeplate

--- prototype/resources/views/mail-sent.blade.php
+++ prototype/resources/views/mail-sent.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
+
 @section('content')
-<p>{{$mailMessage}}</p>
+<div class="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md">
+    <h2 class="text-2xl font-semibold mb-4">New Message</h2>
+    <p class="text-gray-700">{{ $mailMessage }}</p>
+</div>
 @endsection
-
-
